af4fcb4e0088e3827a6ab231d5c6c948461becb2
Pavel Zinoviev <pzinoviev@happynumbers.com>
2025-09-15 20:32:23 +0300

    feat(ui): skills radar widget

--- src/feature/About/index.tsx
+++ src/feature/About/index.tsx
@@ -1,7 +1,10 @@
 import styles from './styles.module.scss';
+
 import { Globe } from '../Globe';
 import { ChartDonut } from '../ChartDonut';
 import { useActivityChartData } from '../../hooks/useLangStatChartData';
+import { RadarChart } from '../RadarChart';
+import { InfoPlate } from '@/shared/InfoPlate';
 
 export const About = () => {
   const { data, isLoading } = useActivityChartData();
@@ -9,24 +12,35 @@ export const About = () => {
   return (
     <section className={styles.about}>
       <div className={styles.aboutWrapper}>
-        <div className={styles.widgetWrapper}>
+        <div className={`${styles.widgetWrapper} ${styles.donutWidgetGap}`}>
           <header className={styles.headerContainer}>
             <h3>Code Palette</h3>
-            <p className={styles.locationText}>
+            <p className={styles.descriptionText}>
               Primary frameworks and languages
               <br />
               used in my work
             </p>
           </header>
-          <div className={styles.donutWrapper}>
-            <ChartDonut data={data} isLoading={isLoading} />
+          <ChartDonut data={data} isLoading={isLoading} />
+          <InfoPlate text={'Data from my public GitHub and private work repositories'} />
+        </div>
+        <div className={styles.widgetWrapper}>
+          <header className={styles.headerContainer}>
+            <h3>Skill Radar</h3>
+            <p className={styles.descriptionText}>
+              Current strengths and growth areas
+              <br />
+              planned for 2026
+            </p>
+          </header>
+          <div className={styles.radarWrapper}>
+            <RadarChart />
           </div>
         </div>
-        <div className={styles.widgetWrapper}></div>
-        <div className={`${styles.widgetWrapper} ${styles.globeWidget}`}>
+        <div className={`${styles.widgetWrapper}`}>
           <header className={styles.headerContainer}>
             <h3>Time Zone</h3>
-            <p className={styles.locationText}>
+            <p className={styles.descriptionText}>
               Located in Moscow. Open to remote
               <br />
               work globally
