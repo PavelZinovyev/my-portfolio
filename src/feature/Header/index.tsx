2556213a0bec7db49a93429c67eddba9e18d0f33
Pavel Zinoviev <frontenderpavel@gmail.com>
2025-09-22 00:13:12 +0300

    feat(chore): custom layers

--- src/feature/Header/index.tsx
+++ src/feature/Header/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
 import styles from './styles.module.scss';
 import { HEADER_LIST } from '../../constants/headerList';
 import { Container } from '../../shared/Container';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
+  const [scrolled, setScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    document.addEventListener('scroll', handleScroll);
+
+    return () => document.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className={styles.header}>
+    <div className={`${styles.header} ${scrolled ? styles.blur : ''}`}>
       <Container>
         <div className={styles.headerWrapper}>
           <a className={styles.logo}>Pavel Zinovyev</a>
