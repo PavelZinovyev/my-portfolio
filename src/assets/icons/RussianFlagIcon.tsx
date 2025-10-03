22df4545ee6fd993141458b8962c34ef317f9702
Pavel Zinoviev <frontenderpavel@gmail.com>
2025-10-03 21:46:09 +0300

    fix(chore): remove unknown dom properties

--- src/assets/icons/RussianFlagIcon.tsx
+++ src/assets/icons/RussianFlagIcon.tsx
@@ -7,7 +7,7 @@ export const RussianFlagIcon = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_503_2726)">
+      <g clipPath="url(#clip0_503_2726)">
         <rect
           x="0.25"
           y="0.25"
@@ -16,7 +16,7 @@ export const RussianFlagIcon = () => {
           rx="1.75"
           fill="white"
           stroke="#F5F5F5"
-          stroke-width="0.5"
+          strokeWidth="0.5"
         />
         <mask id="mask0_503_2726" maskUnits="userSpaceOnUse" x="0" y="0" width="28" height="20">
           <rect
@@ -27,17 +27,17 @@ export const RussianFlagIcon = () => {
             rx="1.75"
             fill="white"
             stroke="white"
-            stroke-width="0.5"
+            strokeWidth="0.5"
           />
         </mask>
         <g mask="url(#mask0_503_2726)">
           <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M0 13.3333H28V6.66667H0V13.3333Z"
             fill="#0C47B7"
           />
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M0 20H28V13.3333H0V20Z" fill="#E53B35" />
+          <path fillRule="evenodd" clipRule="evenodd" d="M0 20H28V13.3333H0V20Z" fill="#E53B35" />
         </g>
       </g>
       <defs>
